ea8df42e408bb8a940c5d1c53f421a9f6743ecc9
HadassaOliveira <hadassaoliveira@alu.ufc.br>
2021-01-08 11:43:19 +0000

    Atv. Ano bissexto

--- main.py
+++ main.py
@@ -3,9 +3,24 @@
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
-    # COLOQUE SEU CÓDIGO AQUI
-    # TODO
+    ano=int(input("Digite um ano:\n"))
 
+    def testebisexto(ano):
+      if (ano%4)==0:
+        if (ano%100)==0:
+          if (ano%400)==0:
+            return True
+          else:
+              return False
+        else:
+           return True
+      else:
+        return False
 
+    if(testebisexto(ano)):
+      print(f"O ano {ano} é bissexto.")
+    else:
+      print(f"O ano {ano} não é bissexto.")
+    
 if __name__ == '__main__':
     main()
